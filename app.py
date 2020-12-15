171c4b2bee519c4d309cc6a77adf1628123b3e8b
Electrimantus <matias.pasonen@gmail.com>
2020-12-15 15:45:03 +0200

    Lisätty Flaskin login

--- app.py
+++ app.py
@@ -22,6 +22,19 @@ class User(Usermixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+#Login Flaskillä.
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
 @app.route('/')
 def index():
     user = User.query.filter_by(username='Niko').first()
