87a6fff09c072594404f751a0e3f13ac0131b064
NikoWLR <wahlroosniko@gmail.com>
2020-12-15 16:04:43 +0200

    Fixed merge errors

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from database import tilat, asiakkaat
+#from database import tilat
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost'
 api = Api(app)
 db = SQLAlchemy(app)
-
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+# Stuff for the log in function
 
 class User(Usermixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,11 +38,46 @@ def logout():
 def home():
     return 'The current user is ' + current_user.username
 
+#Stuff for the Databases
+
+class tilat (db.Model):
+    __tablename__ = 'tyotilat'
+    ttNimi = db.Column(db.String(50), primary_key = True)
+    ttKuvaus = db.Column(db.String(), nullable = False)
+    ttTyyppi = db.Column(db.String(60), nullable=False)
+
+    def __init__(self, ttNimi, ttKuvaus, ttTyyppi):
+        self.ttNimi = ttNimi
+        self.ttKuvaus = ttKuvaus
+        #self.ttTyyppi = ttTyyppi
+
+#"Asiakkaat" -table jossa nimi ja sähköposti
+
+class asiakkaat (db.Model):
+    __tablename__ = 'asiakkaat'
+    AsiakasNimi = db.Column(db.String(50), primary_key = True)
+    AsiakasEmail = db.Column(db.String(), nullable = False)
+
+    def __init__(self, asiakasNimi, asiakasEmail):
+        self.asiakasNimi = asiakasNimi
+        self.asiakasEmail = asiakasEmail
+
 @app.route('/test', methods =['GET'])
 def test():
     return {
     'test':'test1'
     }
+
+#@app.route('/asiakkaat', methods =['GET'])
+#def gasiakkaat():
+#    kaikkiAsiakkaat = asiakkaat.query.all()
+#    output = []
+#    for asiakkaat in kaikkiAsiakkaat:
+#        currAsiakas = {}
+#        currAsiakas ['AsiakasNimi']= asiakkaat.AsiakasNimi
+#        currAsiakas ['AsiakasEmail']= asiakkaat.AsiakasEmail
+#        output.append (currAsiakas)
+#    return jsonify(output)
 
 @app.route('/tilat', methods =['GET'])
 def gtilat():
@@ -50,19 +85,19 @@ def gtilat():
     output = []
     for tyotilat in kaikkitilat:
         currTila = {}
-        currTila ['ttNimi']= tyotilat.ttNimi
-        currTila ['ttKuvaus']= tyotilat.ttKuvaus
-        currTila['ttTyyppi'] = tyotilat.ttTyyppi
+        currTila ['ttNimi']=tyotilat.ttNimi
+        currTila ['ttKuvaus']=tyotilat.ttKuvaus
+        currTila['ttTyyppi']=tyotilat.ttTyyppi
         output.append (currTila)
     return jsonify(output)
 
 @app.route('/tilat', methods =['POST'])
-def posttilat():
-    tilaData = request.get_json(silent=True)
-    tila = tilat(ttNimi=tilaData['ttNimi'], ttKuvaus=tilaData['ttKuvaus'], ttTyyppi=tilaData['ttTyyppi'])
+def postTilat():
+    tiladata = request.get_json()
+    tila = tilat(ttNimi=tiladata['ttNimi'], ttKuvaus=tiladata['ttKuvaus'], ttTyyppi=tiladata['ttTyyppi'])
     db.session.add(tila)
-    db.session.commit
-    return jsonify(tilaData)
+    db.session.commit(tila)
+    return jsonify(tiladata)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
