3ba192dd31f36a117eac6a075a025955f94e22d2
Electrimantus <matias.pasonen@gmail.com>
2020-12-15 16:24:49 +0200

    Tarkennettu moduulit sekä lisätty loginiin luokat

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from database import tilat, asiakkaat
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'Admin'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost'
+bootstrap = Bootstrap(app)
 api = Api(app)
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class User(Usermixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-#Login Flaskillä.
+#Login Flaskillä. Eli mitä tietoja kysyttyihin "Fieldeihin" voidaan syöttää.
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
