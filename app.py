d87a8edd0a55eb2bbb5be9519fc4475b02e27c1f
Cettu <waltter.kettunen@edu.turkuamk.fi>
2020-12-15 15:35:32 +0200

    login-settiä, uusi yritys

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from database import tilat, asiakkaat
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
@@ -9,6 +10,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost'
 api = Api(app)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(Usermixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    user = User.query.filter_by(username='Niko').first()
+    login_user(user)
+    return 'You are now logged in!'
+
+@app.route('logout')
+@login_required
+def logout():
+    return 'You are now logged out!'
+
+@app.route('/home')
+@login_required
+def home():
+    return 'The current user is ' + current_user.username
 
 @app.route('/test', methods =['GET'])
 def test():
