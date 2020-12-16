ac997dc09f3a6fbc10935a7e63634a8a06238cb2
Electrimantus <matias.pasonen@gmail.com>
2020-12-17 00:58:04 +0200

    Viimeistely esitystä varten.

--- venv/app.py
+++ venv/app.py
@@ -20,7 +20,7 @@ db = SQLAlchemy(app)                                                            
 login_manager = LoginManager()
 login_manager.init_app(app)
 auth = HTTPBasicAuth()
-mail = Mail(app)
+mail = Mail(app) #Ei varsinaisessa käytössä, koska Email-domainia ei ole olemassa.
 
 
 
@@ -35,7 +35,8 @@ number_of_tables=24
 def load_user(user_id):
     return User.get(user_id)
 
-#
+#Callback toiminto, joka tarkistaa kahden argumentin yhteensopivuuden. Olemme käyttäneen
+#asiakkaa db-modelissa hash_password callbackia, joten tätä ei tarvita. (periaatteessa)
 
 @auth.verify_password
 def verify_password(telephone, password):
@@ -45,7 +46,8 @@ def verify_password(telephone, password):
             return False
     g.user = user
     return True
-#Virheiden varalle toimito, joka palauttaa muokatun 404-sivun "I Love Bugs"
+
+#Tämä route palauttaa virhesivun, jos esim salasana ei täsmää tai käytetään vanhaa versiointia.
 
 @app.errorhandler(404)
 def user_loader(error):
@@ -55,11 +57,11 @@ def user_loader(error):
 
 
 
-#Alkavassa vihreessä tekstissä on osoita, joita voidaan lainata tarpeen mukaan.  Jos tulee olemaan vain yksi admin-käyttis,
+#Alkavassa koodissa on osoita, joita voidaan lainata tarpeen mukaan.  Jos tulee olemaan vain yksi admin-käyttis,
 #Ei alla oleva app.route ole kovin oleellinen.
 
 
-# Reitti, jota pitkin uusi käyttäjä lisätään. Käyttäjällä voi kyllä olla useampi sähköposti.
+# Route, jota pitkin uusi käyttäjä lisätään. Käyttäjällä voi kyllä olla useampi sähköposti.
 @app.route('/api/user/add', methods=['POST'])
 def new_user():
     telephone = request.json.get('telephone')
@@ -76,6 +78,7 @@ def new_user():
     db.session.commit()
     return (jsonify({'status': 'User added successfully!'}), 201)
 
+#Get-pyyntö käyttäjän id:tä varten. Jos ei pysty hakemaan db:stä, palauttaa bad requestin.
 
 @app.route('/api/user/<int:id>')
 def get_user(id):
@@ -84,12 +87,14 @@ def get_user(id):
         abort(400)
     return jsonify({'telephone': user.telephone})
 
+#DB:n tietoihin pääsy kirjautuneena.
 
 @app.route('/api/resource')
 @auth.login_required
 def get_resource():
     return jsonify({'data': 'Hello, %s!' % g.user.telephone})
 
+#Tätä kautta käyttäjä voi suorittaa varauspyynnön, vain kun on kirjautuneena varausjärjestelmään.
 
 @app.route('/api/reservation', methods=['POST'])
 @auth.login_required
@@ -100,22 +105,24 @@ def make_reservation():
     # getting user email
     user = User.query.filter_by(telephone=user_id).first()
 
-    # Processing date and creating python datetime object - date:n prosessointi ja datetime objektin luominen.
+    #Määritetään aika-objekti ja liitetään se varausmuuttujaan.
     date_obj = datetime.strptime(date, '%Y-%m-%d')
     reservations = Reservation.query.filter_by(date=date_obj).count()
 
     if reservations < number_of_tables:
-        # More reservations can be made. - On mahdollisa tehdä lisää varauksia.
+        #Jos varausten määrä on pienempi, kuin vapaana olevien tilojen määrä, varaus voidaan suorittaa.
         reservation = Reservation(telephone=user_id, date=date_obj)
         db.session.add(reservation)
         db.session.commit()
 
-        # Sending the email. - Sähköpostin lähetys käyttäjälle.
+        #Jos varaajan pyyntö menee läpi, voidaan sähköpostista lähettää automaattivastaus.
+        #S.posti-palvelinta ei ole kuitenkaan määritetty, koska mitään sopivaa domainia ei ole olemassa/
+        #Eikä tämä ole projektin ajanhallinnan kannalta välttämätön ominaisuus.
         try:
-            msg = Message("Reservation made successfully!",
-                          sender="restaurant@gmail.com",
+            msg = Message("Workspace reserved, Thank you!",
+                          sender="info@TUASspacemanger.fi",
                           recipients=[user.email])
-            message_string = """Hi {0},\nYou have made a reservation successfully in our restaurant on {1}.\nFeel free to contact us for any inquiries. Thank you.\nRestaurant Staff"""
+            message_string = """Hello {0},\nReservation has been made on {1}.\n For any questions, reply to this mail. \nTUAS Workspace reservation Team."""
             msg.body = message_string.format(user.name, date)
             mail.send(msg)
             return (jsonify({'status': 'Reservation added successfully'}))
@@ -123,16 +130,29 @@ def make_reservation():
         except Exception as e:
             return (jsonify({'status': 'Reservation added successfully but email not sent'}))
 
-
+        #Jos varausta ei voida suorittaa, toiminta palauttaa viestin.
     else:
-        # Cannot make anymore reservations. - Ei voi tehdä enempää varauksia.
-        return (jsonify({
-                            'status': 'Reservation adding failed !. We cannot accomodate any more reservations for the mentioned date'}))
-# Stuff for the log in function
+        return (jsonify({'status': 'Cannot make reservation, workspace not available.'}))
+
+#Flask-loginiin tarvitta käyttäjän db.model. UserMixin tarkoitus on implementoida
+#kirjautumiseen tarvittavia ominaisuuksi, jotta esimerkiksi is_authenticated voidaan
+#tuoda ja tarkistaa kirjautumistiedot. Helpottaa, sillä kaikkia metodeita ei tarvitse kirjoittaa itse.
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User{}>'.format(self.username)
 
 
 #Asiakkaat-olion luominen ja siihen tarvittava data. Salasanan tallentaminen tekstinä on epäturvallista, joten
